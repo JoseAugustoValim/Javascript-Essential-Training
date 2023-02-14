41ea208a5b84cb9444e881f7e5cd474b2a42dd43
Jose Valim <joseaugustovalim1994@gmail.com>
2023-02-14 10:11:08 -0300

    feature: Exercicio criar objetos

---
+++ Objects/Practice1.js
@@ -0,0 +1,48 @@
+/**
+ * Create a chakra object.
+ */
+
+const chakra = {
+  nome: 'Chakra Padrão',
+  id: '0',
+  forma: 'Forma Padrão',
+  elemento: 'Elemento Padrão',
+  tipo: 'Tipo Padrão',
+  outroTipo: 'Outro Tipo Padrão',
+  corChakra: {
+    nomeCor: 'Nome Padrao',
+    outraCor: 'Outro tipo de cor',
+  },
+};
+
+/**
+ * Create a haki object.
+ */
+
+const haki = {
+  tipoHaki: 'Haki padrao',
+};
+
+/**
+ * Create a demon-fruit object.
+ */
+
+const demonfruit = {
+  crescimento: 'Crescimento Padrao',
+  identificacao: 'Identificação Padrao',
+  proficiencia: 'Proficiaencia padrao',
+  forca: {
+    beneficio: 'beneficio padrao',
+    consequencia: 'consequencia padrao',
+  },
+  fraqueza: {
+    beneficio: 'beneficio padrao',
+    consequencia: 'consequencia padrao',
+  },
+  superioridade: {
+    idSuperioridade: '0',
+  },
+  pesquisa: {
+    arficialidade: 'sim',
+  },
+};
